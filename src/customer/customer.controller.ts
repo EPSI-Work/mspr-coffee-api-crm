9f7a61fd7e5448959f04e631afca2fa6561226c2
dimit <dimitri.ollivier14@gmail.com>
2023-03-21 21:18:41 +0100

    feat(customer) add logic

--- src/customer/customer.controller.ts
+++ src/customer/customer.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Post } from "@nestjs/common"
-import { CustomerService } from "./customer.service";
+import { Controller, Post, Get, Body } from "@nestjs/common"
+import { CustomerService } from "./customer.service"
+import { CustomerDto } from "./dto"
 
 @Controller('customer')
 export class CustomerController {
     constructor(private CustomerService: CustomerService) {}
 
-    @Post('getCustomer')
-    getCustomer() {
-        return 'test'
+
+    @Post()
+    addCustomer(@Body() dto: CustomerDto) {
+        return this.CustomerService.addCustomer(dto)
+    }
+
+    @Get()
+    getCustomerById(@Body() id: number) {
+        return id
     }
 }
